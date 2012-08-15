d2352d5dee0b988b96c89eecb0858987ca1e7da5
jasonchi <jasonchi@live.cn>
2012-08-15 23:44:37 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mztop::Application.config.secret_token = '68df54dc446f813de4909740d58bb16fd916d4f568056b893e76b65f2b8eac1441e3f0abf76aef96ac4fd99d7011d4b1022839a12b3dddc642c138cb34667bda'
